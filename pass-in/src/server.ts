61daa67c618811b0beaa6f104d33c4575d2bd0a6
Mari2213 <mariannasilvapereira15@gmail.com>
2024-07-15 21:44:33 -0300

    create logical

--- pass-in/src/server.ts
+++ pass-in/src/server.ts
@@ -1,15 +1,36 @@
 import fastify from "fastify";
+import { z } from "zod";
+import { PrismaClient } from "@prisma/client";
 
 const app = fastify();
 
-app.get("/", () => {
-  return "hello";
+const prisma = new PrismaClient({
+  log: ["query"],
 });
 
-app.get("/teste", () => {
-  return "hello teste";
+app.post("/events", async (request, reply) => {
+  const createEventSchema = z.object({
+    title: z.string().min(3),
+    details: z.string().nullable(),
+    maxinumAttendees: z.number().int().positive().nullable(),
+  });
+
+  const data = createEventSchema.parse(request.body);
+  console.log(data);
+
+  const events = await prisma.event.create({
+    data: {
+      title: data.title,
+      details: data.details,
+      maximumAttendees: data.maxinumAttendees,
+      slug: new Date().toISOString(),
+    },
+  });
+
+  return { eventId: events.id };
+  // return reply.status(201).send({ eventId: events.id });
 });
 
 app.listen({ port: 3333 }).then(() => {
-  console.log("http server running");
+  console.log("http server running!!");
 });
